14bbeb9e108b7df52531f81e8a4e7efdb35da770
sahara.ap <sahara.ap13@gmail.com>
2023-08-08 14:45:21 +0300

    добавили brief-view(сводка путешествий)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+import './views/brief-view.js';
+
+/**
+ * @type {import('./views/brief-view').default}
+ */
+const briefView = document.querySelector('brief-view');
+briefView.render();
