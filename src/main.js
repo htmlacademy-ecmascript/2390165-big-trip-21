0cfaadd4680f82235d4e8d3b8c669f3d04d97aa9
sahara.ap <sahara.ap13@gmail.com>
2023-08-08 15:24:22 +0300

    Добавили sortView

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import './views/brief-view.js';
 import './views/filter-view.js';
+import './views/add-button-view.js';
+import './views/sort-view.js';
+
 
 /**
  * @type {import('./views/brief-view').default}
@@ -11,5 +14,17 @@ const briefView = document.querySelector('brief-view');
  */
 const filterView = document.querySelector('filter-view');
 
+/**
+ * @type {import('./views/add-button-view').default}
+ */
+const addButtonView = document.querySelector('add-button-view');
+
+/**
+ * @type {import('./views/sort-view').default}
+ */
+const sortView = document.querySelector('sort-view');
+
 briefView.render();
 filterView.render();
+addButtonView.render();
+sortView.render();
