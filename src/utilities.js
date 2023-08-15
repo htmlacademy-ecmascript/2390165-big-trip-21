b813f66fb5eed5afeb938a4cf9a92bc09bd2b402
sahara.ap <sahara.ap13@gmail.com>
2023-08-15 13:51:59 +0300

    Добавили функцию для форматирования дат

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,15 @@
+import dayjs from 'dayjs';
+
+/**
+ * @param {dayjs.ConfigType} value
+ * @returns {string}
+ */
+function formatDate(value) {
+  return dayjs(value).format('MMM D');
+}
+
+console.log(formatDate('2023-03-18'));
+
 /**
  * @param {TemplateStringsArray} strings
  * @param {...any} values
@@ -19,4 +31,4 @@ function html(strings, ...values) {
   });
 }
 
-export {html};
+export {html, formatDate};
