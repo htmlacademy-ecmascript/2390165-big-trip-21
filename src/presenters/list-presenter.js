20d22c3505be09625181ac797f0639d5393cae57
sahara.ap <sahara.ap13@gmail.com>
2023-08-25 14:16:18 +0300

    Добавили в ListPresenter метод создания PointModel из ItemState

--- src/presenters/list-presenter.js
+++ src/presenters/list-presenter.js
@@ -49,7 +49,7 @@ class ListPresenter extends Presenter {
 
         offers: offers.map((offer) => ({
           ...offer,
-          isSelected: point.offersIds?.includes(offer.id)
+          isSelected: point.offerIds?.includes(offer.id)
         })),
 
         isFavorite: point.isFavorite,
@@ -59,6 +59,29 @@ class ListPresenter extends Presenter {
 
     this.view.setState({items});
   }
+
+  /**
+   * @param {import('../views/list-view').ItemState} state
+   * @returns {import('../models/point-model').default}
+   */
+  createPoint(state) {
+    const point = this.model.createPoint();
+
+
+    Object.assign(point, {
+      id: state.id,
+      type: state.types.find((type) => type.isSelected).value,
+      destinationId: state.destinations.find((destination) => destination.isSelected)?.id,
+      dateFrom: state.dateFrom,
+      dateTo: state.dateTo,
+      basePrice: state.basePrice,
+      offerIds: state.offers.filter((offer) => offer.isSelected).map((offer) => offer.id),
+      isFavorite: state.isFavorite
+    });
+
+    return point;
+  }
+
 
   /**
    * @param {CustomEvent & {
@@ -91,7 +114,7 @@ class ListPresenter extends Presenter {
 
     card.state.isFavorite = !card.state.isFavorite;
     //TODO: Обновить модель
-    console.log(this.model.createPoint());
+    console.log(this.createPoint(card.state));
 
     card.render();
   }
