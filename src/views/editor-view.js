21d50f90db0eb06ce51bd436d91b716a08af388a
sahara.ap <sahara.ap13@gmail.com>
2023-09-01 13:54:24 +0300

    Реализована в EditorView отправка события 'edit'

--- src/views/editor-view.js
+++ src/views/editor-view.js
@@ -12,6 +12,7 @@ class EditorView extends View {
     super();
 
     this.addEventListener('click', this.onClick);
+    this.addEventListener('change', this.onChange);
   }
 
   connectedCallback() {
@@ -280,6 +281,15 @@ class EditorView extends View {
     if (event.key?.startsWith('Esc')) {
       this.dispatch('close');
     }
+  }
+
+  /**
+   * @param {Event & {
+   *  target: HTMLInputElement
+   * }} event
+   */
+  onChange(event) {
+    this.dispatch('edit', event.target);
   }
 
 }
