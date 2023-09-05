2a39e0b3f4741f800bb9507ea542824599262ab8
sahara.ap <sahara.ap13@gmail.com>
2023-09-05 15:04:31 +0300

    Реализовали запрет выбора дат в несуществующем диапазоне

--- src/utilities.js
+++ src/utilities.js
@@ -23,8 +23,13 @@ function createCalendars(inputFrom, inputTo) {
     'time_24hr': true
   };
 
+
   const calendarFrom = flatpickr(inputFrom, options);
   const calendarTo = flatpickr(inputTo, options);
+
+  calendarFrom.set('onChange', ([date]) => calendarTo.set('minDate', date));
+  calendarTo.set('minDate', calendarFrom.selectedDates.at(0));
+
 
   return () => {
     calendarFrom.destroy();
