709e7486b57fb08803166bed60bff31e022409cf
sahara.ap <sahara.ap13@gmail.com>
2023-09-15 15:21:49 +0300

    Добавили функцию для экранирования данных

--- src/utilities.js
+++ src/utilities.js
@@ -2,6 +2,7 @@ import 'flatpickr/dist/flatpickr.css';
 import flatpickr from 'flatpickr';
 import dayjs from 'dayjs';
 import durationPlugin from 'dayjs/plugin/duration.js';
+import {escape} from 'he';
 
 dayjs.extend(durationPlugin);
 
@@ -102,6 +103,30 @@ function html(strings, ...values) {
   });
 }
 
+/**
+ * @param {any} data
+ * @returns {any}
+ */
+function sanitize(data) {
+  switch (data?.constructor) {
+    case String:
+      return escape(data);
+
+    case Array:
+      return data.map(sanitize);
+
+    case Object:
+      return Object.keys(data).reduce((copy, key) => {
+        copy[key] = sanitize(data[key]);
+
+        return copy;
+      }, {});
+
+    default:
+      return data;
+  }
+}
+
 export {
   createCalendars,
   formatDate,
@@ -109,4 +134,5 @@ export {
   formatDuration,
   formatNumber,
   html,
+  sanitize,
 };
